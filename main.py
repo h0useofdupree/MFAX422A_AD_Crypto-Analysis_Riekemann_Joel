b80872947196c495fcdb2f0ef092d1afedf97273
h0useofdupree <joel.riekemann@gmail.com>
2024-04-29 09:59:30 +0200

    Add heatmap for correlation matrix

--- main.py
+++ main.py
@@ -2,9 +2,17 @@ import sys
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QVBoxLayout, QWidget, QFileDialog, QHBoxLayout
+from PyQt5.QtWidgets import (
+    QApplication,
+    QMainWindow,
+    QPushButton,
+    QVBoxLayout,
+    QWidget,
+    QFileDialog,
+    QHBoxLayout,
+)
 from PyQt5.QtGui import QIcon
-from PyQt5.QtCore import QSize
+
 
 class DataAnalyzer(QMainWindow):
     def __init__(self):
@@ -12,8 +20,8 @@ class DataAnalyzer(QMainWindow):
         self.initUI()
 
     def initUI(self):
-        self.setWindowTitle('Data Visualization App')
-        self.setWindowIcon(QIcon('app_icon.png'))  # Optionally set an icon
+        self.setWindowTitle("Data Visualization App")
+        self.setWindowIcon(QIcon("app_icon.png"))  # Optionally set an icon
         self.setGeometry(100, 100, 360, 200)  # Adjust window size and position
 
         outer_layout = QVBoxLayout()
@@ -23,51 +31,71 @@ class DataAnalyzer(QMainWindow):
         widget.setLayout(outer_layout)
         self.setCentralWidget(widget)
 
-        # Define button styles
         button_style = "QPushButton { font-size: 16px; margin: 5px; padding: 10px; }"
 
-        # Line Graph Button
-        btn_line = QPushButton('Line Graph', self)
+        btn_line = QPushButton("Line Graph", self)
         btn_line.setStyleSheet(button_style)
-        btn_line.clicked.connect(lambda: self.open_file('line'))
+        btn_line.clicked.connect(lambda: self.open_file("line"))
         button_layout.addWidget(btn_line)
 
-        # Bar Chart Button
-        btn_bar = QPushButton('Bar Chart', self)
-        btn_bar.setStyleSheet(button_style)
-        btn_bar.clicked.connect(lambda: self.open_file('bar'))
-        button_layout.addWidget(btn_bar)
+        btn_corr = QPushButton("Korrelations-Heatmap", self)
+        btn_corr.setStyleSheet(button_style)
+        btn_corr.clicked.connect(self.plot_correlation_heatmap)
+        button_layout.addWidget(btn_corr)
 
-        # Scatter Plot Button
-        btn_scatter = QPushButton('Scatter Plot', self)
-        btn_scatter.setStyleSheet(button_style)
-        btn_scatter.clicked.connect(lambda: self.open_file('scatter'))
-        button_layout.addWidget(btn_scatter)
+        # btn_bar = QPushButton("Bar Chart", self)
+        # btn_bar.setStyleSheet(button_style)
+        # btn_bar.clicked.connect(lambda: self.open_file("bar"))
+        # button_layout.addWidget(btn_bar)
+        #
+        # btn_scatter = QPushButton("Scatter Plot", self)
+        # btn_scatter.setStyleSheet(button_style)
+        # btn_scatter.clicked.connect(lambda: self.open_file("scatter"))
+        # button_layout.addWidget(btn_scatter)
 
         outer_layout.addLayout(button_layout)
 
     def open_file(self, graph_type):
         options = QFileDialog.Options()
-        file_name, _ = QFileDialog.getOpenFileName(self, "Open CSV File", "", "CSV Files (*.csv)", options=options)
+        file_name, _ = QFileDialog.getOpenFileName(
+            self, "Open CSV File", "", "CSV Files (*.csv)", options=options
+        )
         if file_name:
             self.plot_data(file_name, graph_type)
 
+    def plot_correlation_heatmap(self):
+        options = QFileDialog.Options()
+        file_name, _ = QFileDialog.getOpenFileName(
+            self, "Open CSV File", "", "CSV Files (*.csv)", options=options
+        )
+
+        if file_name:
+            data = pd.read_csv(file_name)
+            data["snapped_at"] = pd.to_datetime(data["snapped_at"])
+
+            corr_matrix = data.corr()
+            sns.heatmap(corr_matrix, annot=True, cmap="coolwarm")
+            plt.show()
+
     def plot_data(self, file_path, graph_type):
-        data = pd.read_csv(file_path, parse_dates=['snapped_at'], index_col='snapped_at')
+        data = pd.read_csv(
+            file_path, parse_dates=["snapped_at"], index_col="snapped_at"
+        )
         plt.figure(figsize=(10, 5))
 
-        if graph_type == 'line':
-            sns.lineplot(x=data.index, y=data['price'], marker='o', linestyle='-')
-        elif graph_type == 'bar':
-            plt.bar(data.index, data['price'], color='blue')
-        elif graph_type == 'scatter':
-            plt.scatter(data.index, data['price'], color='red')
+        if graph_type == "line":
+            sns.lineplot(x=data.index, y=data["price"], marker="o", linestyle="-")
+        elif graph_type == "bar":
+            plt.bar(data.index, data["price"], color="blue")
+        elif graph_type == "scatter":
+            plt.scatter(data.index, data["price"], color="red")
 
-        plt.title(f'{graph_type.capitalize()} of Price Data Over Time')
-        plt.xlabel('Date')
-        plt.ylabel('Price')
+        plt.title(f"{graph_type.capitalize()} of Price Data Over Time")
+        plt.xlabel("Date")
+        plt.ylabel("Price")
         plt.grid(True)
         plt.show()
+
 
 def main():
     app = QApplication(sys.argv)
@@ -75,5 +103,6 @@ def main():
     ex.show()
     sys.exit(app.exec_())
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
